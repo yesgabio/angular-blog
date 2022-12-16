0b51917caeb36158c9f88712d756b314f0bc0bbc
Gabriela Souza Oliveira <10088640@pucrs.br>
2022-12-16 09:54:27 -0300

    components styles

---
+++ src/app/components/small-text-card/small-text-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-small-text-card',
+  templateUrl: './small-text-card.component.html',
+  styleUrls: ['./small-text-card.component.css']
+})
+export class SmallTextCardComponent {
+
+}
